d3434af09a099d6f8b60d3bde6d9c0912e6734bf
Poliyka <poliyka.hsu@gmail.com>
2024-06-30 21:44:46 +0800

    feat: more keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,12 +5,27 @@
 local map = vim.keymap.set
 local umap = vim.keymap.del
 
+-- motion
 map("i", "jj", "<Esc>", { desc = "Escape to normal", remap = false })
+
+-- 快速至行首及行尾
+map({ "n", "v" }, "H", "^", { desc = "Go to first word", remap = false })
+map({ "n", "v" }, "L", "$", { desc = "Go to last word", remap = false })
+
+-- 切換 Tab
+map("n", "gt", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next Tab", noremap = true })
+map("n", "gT", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Prev Tab", noremap = true })
+
+-- Shift + Alt 複製本行至 action <j, k>
+map("n", "<S-M-j>", ":.t.<CR>", { noremap = true, silent = true })
+map("n", "<S-M-k>", ":t-1<CR>", { noremap = true, silent = true })
 
 -- display variable/function information
 map("n", "K", "<Cmd>Lspsaga hover_doc<CR>")
+
 -- go to function
 map("n", "gf", "<Cmd>Lspsaga goto_definition<CR>")
+
 -- List variable/function
 -- open keymap:'o'
 map("n", "fd", "<Cmd>Lspsaga finder tyd+ref+imp+def<CR>")
@@ -44,3 +59,18 @@ map("v", "sp", '"dpgvy', { remap = true, silent = true })
 map("v", "sP", '"dPgvy', { remap = true, silent = true })
 map({ "n", "v" }, "<leader><leader>p", '"+p', { remap = true, silent = true })
 map({ "n", "v" }, "<leader><leader>P", '"+P', { remap = true, silent = true })
+
+-- CamelCaseMotion
+map("n", "gw", "<Plug>CamelCaseMotion_w")
+map("n", "gb", "<Plug>CamelCaseMotion_b")
+map("n", "ge", "<Plug>CamelCaseMotion_ge")
+
+-- Delete Camel
+map("n", "di<leader>w", "d<Plug>CamelCaseMotion_iw")
+map("n", "di<leader>b", "d<Plug>CamelCaseMotion_ib")
+map("n", "di<leader>e", "d<Plug>CamelCaseMotion_ie")
+
+-- Select Camel
+map("n", "vi<leader>w", "v<Plug>CamelCaseMotion_iw")
+map("n", "vi<leader>b", "v<Plug>CamelCaseMotion_ib")
+map("n", "vi<leader>e", "v<Plug>CamelCaseMotion_ie")
--- lua/plugins/plugs.lua
+++ lua/plugins/plugs.lua
@@ -8,11 +8,11 @@ return {
   },
   { "onsails/lspkind-nvim" },
   {
-    -- "bkad/CamelCaseMotion",
-    -- vim.keymap.set("", "w", "<Plug>CamelCaseMotion_w", { silent = true }),
-    -- vim.keymap.set("", "b", "<Plug>CamelCaseMotion_b", { silent = true }),
-    -- vim.keymap.set("", "e", "<Plug>CamelCaseMotion_e", { silent = true }),
-    -- vim.keymap.set("", "ge", "<Plug>CamelCaseMotion_ge", { silent = true }),
+    "bkad/CamelCaseMotion",
+  },
+  {
+    "ggandor/leap.nvim",
+    enabled = true,
   },
   -- EasyMotion
   {
